c08c287b4a44f4953d8b60aa1bad2c1a7a5fe9a6
jasonstevens13 <jasonstevens13@yahoo.com>
2020-07-20 13:09:00 -0400

    create react app bootstrapped; beginning to refactor

---
+++ portfolio/src/components/NavTabs.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function NavTabs(props) {
+  return (
+    <ul className="nav nav-tabs">
+
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => props.handlePageChange("About")}
+          className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#projects"
+          onClick={() => props.handlePageChange("Projects")}
+          className={props.currentPage === "Projects" ? "nav-link active" : "nav-link"}
+        >
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => props.handlePageChange("Contact")}
+          className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
